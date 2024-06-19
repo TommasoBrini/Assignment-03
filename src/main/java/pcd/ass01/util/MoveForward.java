cacb14ce00174e8feb93ef5f1ce24ff51b6b4d09
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-06-19 16:46:10 +0200

    delete simulation package

--- src/main/java/pcd/ass01/util/MoveForward.java
+++
@@ -1,8 +0,0 @@
-package pcd.ass01.util;
-
-import pcd.ass01.agent.Action;
-
-/**
- * Car agent move forward action
- */
-public record MoveForward(String agentId, double distance) implements Action {}
