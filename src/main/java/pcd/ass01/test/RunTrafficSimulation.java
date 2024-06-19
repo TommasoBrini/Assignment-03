b508971a133a23b6a42a12c2bdd5f3ef25191a69
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-06-19 15:16:10 +0200

    add simulation from assignement01

---
+++ src/main/java/pcd/ass01/test/RunTrafficSimulation.java
@@ -0,0 +1,32 @@
+package pcd.ass01.test;
+
+import pcd.ass01.simulation.SimulationThread;
+import pcd.ass01.simulation.implementation.*;
+
+/**
+ * 
+ * Main class to create and run a simulation
+ * 
+ */
+public class RunTrafficSimulation {
+
+	public static void main(String[] args) {
+		//var simulation = new TrafficSimulationSingleRoadTwoCars();
+		//var simulation = new TrafficSimulationSingleRoadSeveralCars();
+		//var simulation = new TrafficSimulationSingleRoadWithTrafficLightTwoCars();
+		var simulation = new TrafficSimulationWithCrossRoads();
+		simulation.setup();
+
+		SimulationThread simulationThread = new SimulationThread(simulation);
+		String name = simulation.getClass().getName();
+		String[] as = name.split("\\.");
+		name = as[as.length - 1];
+
+		RoadSimStatistics stat = new RoadSimStatistics();
+		RoadSimView view = new RoadSimView(simulationThread, name);
+		view.display();
+		simulation.addSimulationListener(stat);
+		simulation.addSimulationListener(view);		
+		simulation.init();
+	}
+}
