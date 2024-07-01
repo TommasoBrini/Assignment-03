c74dbfd1ca637822b10352d291c262500c871284
Tommaso <tommaso.brini@studio.unibo.it>
2024-07-01 19:22:51 +0200

    Import example for part2A

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     // https://mvnrepository.com/artifact/com.typesafe.akka/akka-actor-typed
     implementation("com.typesafe.akka:akka-actor-typed_2.12:2.8.4")
+    implementation("com.rabbitmq:amqp-client:5.17.1")
 }
 
 tasks.test {
