533e201f75f44610e94671af3e0d745515324d31
Riccardo Rambaldi <riccardo.rambaldi4@studio.unibo.it>
2024-07-08 14:38:50 +0200

    feat: add start, stop and pause buttons to part1

--- src/main/java/org/simulation/part1/run/RunTrafficSimulation.java
+++ src/main/java/org/simulation/part1/run/RunTrafficSimulation.java
@@ -1,6 +1,7 @@
 package org.simulation.part1.run;
 
 import org.simulation.part1.concreteSimulation.*;
+import org.simulation.part1.controller.Controller;
 
 /**
  * 
@@ -9,22 +10,14 @@ import org.simulation.part1.concreteSimulation.*;
  */
 public class RunTrafficSimulation {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 
-
-		var simulation = new TrafficSimulationSingleRoadTwoCars();
-		//var simulation = new TrafficSimulationWithCrossRoads();
+		//var simulation = new TrafficSimulationSingleRoadTwoCars();
+		var simulation = new TrafficSimulationWithCrossRoads();
 		//var simulation = new TrafficSimulationSingleRoadSeveralCars();
 		//var simulation = new TrafficSimulationSingleRoadWithTrafficLightTwoCars();
+
 		simulation.setup();
-
-		RoadSimStatistics stat = new RoadSimStatistics(simulation.getNumCars());
-		RoadSimView view = new RoadSimView();
-		view.display();
-
-		simulation.addSimulationListener(stat);
-		simulation.addSimulationListener(view);
-
-		simulation.run(1000);
+		Controller controller = new Controller(simulation, new RoadSimView());
 	}
 }
