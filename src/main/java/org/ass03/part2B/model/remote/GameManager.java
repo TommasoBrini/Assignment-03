64d4b0ca27786df72c686a67f5b7794986543d02
Tommaso <tommaso.brini@studio.unibo.it>
2024-07-09 16:23:15 +0200

    feat: create GameManager remote object

---
+++ src/main/java/org/ass03/part2B/model/remote/GameManager.java
@@ -0,0 +1,25 @@
+package org.ass03.part2B.model.remote;
+
+import org.ass03.part2B.model.Grid;
+
+import java.awt.Color;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.rmi.Remote;
+
+public interface GameManager extends Remote {
+
+    void createGrid() throws RemoteException;
+
+    void updateGrid(int gridId, int row, int col, int value) throws RemoteException;
+
+    void selectCell(int gridId, int row, int col, String idUser, Color color) throws RemoteException;
+
+    void unselectCell(int gridId, int row, int col, String idUser) throws RemoteException;
+
+    void submitGrid(int gridId, String idUser) throws RemoteException;
+
+    List<Grid> getAllGrids() throws RemoteException;
+
+    Grid getGrid(int gridId) throws RemoteException;
+}
---
+++ src/main/java/org/ass03/part2B/model/remote/GameManagerImpl.java
@@ -0,0 +1,53 @@
+package org.ass03.part2B.model.remote;
+
+import org.ass03.part2B.model.Grid;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameManagerImpl implements GameManager{
+
+    private final List<Grid> allGrids;
+
+    public GameManagerImpl() {
+        this.allGrids = new ArrayList<>();
+    }
+
+    @Override
+    public void createGrid() {
+        int gridId = allGrids.size();
+        Grid grid = new Grid(gridId);
+        allGrids.add(grid);
+    }
+
+    @Override
+    public void updateGrid(int gridId, int row, int col, int value) {
+        allGrids.get(gridId).setCellValue(row, col, value);
+    }
+
+    @Override
+    public void selectCell(int gridId, int row, int col, String idUser, Color color) {
+        allGrids.get(gridId).setColor(row, col, color);
+    }
+
+    @Override
+    public void unselectCell(int gridId, int row, int col, String idUser) {
+        allGrids.get(gridId).setColor(row, col, Color.WHITE);
+    }
+
+    @Override
+    public void submitGrid(int gridId, String idUser) {
+        allGrids.get(gridId).setCompleted();
+    }
+
+    @Override
+    public List<Grid> getAllGrids() {
+        return allGrids;
+    }
+
+    @Override
+    public Grid getGrid(int gridId) {
+        return allGrids.get(gridId);
+    }
+}
