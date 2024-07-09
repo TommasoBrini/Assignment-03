55e9ede54bf30cef35e95cfb42bac1313ca74a20
Tommaso <tommaso.brini@studio.unibo.it>
2024-07-09 14:16:31 +0200

    feat: modify build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    // https://mvnrepository.com/artifact/com.typesafe.akka/akka-actor-typed
     implementation("com.typesafe.akka:akka-actor-typed_2.12:2.8.4")
     implementation("com.rabbitmq:amqp-client:5.17.1")
-    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot
     implementation("org.springframework.boot:spring-boot:3.3.1")
 }
 
